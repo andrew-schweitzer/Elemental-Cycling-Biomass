d25986675a0059e8db5e0fa57ad6d993df3d8f21
Anderw <andrewschweitzer0@gmail.com>
2021-11-01 16:33:32 -0400

    Removed accretion

---
+++ Elemental-Cycling-Biomass/src/classes.jl
@@ -0,0 +1,24 @@
+using Pkg
+Pkg.add("Classes")
+Pkg.add("Parameters")
+using Parameters
+using Classes
+
+#creates common structure for all resevoirs with initial values set at 0.0
+@with_kw struct Resevoir
+    volume::BigFloat = 0.0
+    mass::BigFloat = 0.0
+    Nitrogen::BigFloat = 0.0
+    CarbonDioxide::BigFloat = 0.0
+    Oxygen::BigFloat = 0.0
+    Phosphorous::BigFloat = 0.0
+end
+
+Ocean = Resevoir(volume = 1.3e18, Nitrogen = 2.4e16)
+CrustOceanic = Resevoir(Nitrogen = 3.4e18)
+CrustContinential = Resevoir(Nitrogen = 3.4e18)
+Atmosphere = Resevoir(mass = 5.2e18,Nitrogen = 4e18,Phosphorous=0)
+Mantle = Resevoir(Nitrogen = BigFloat(2.8e19))
+
+#this metric will be used to assure that no mass is created nor lost
+PlanetMass = 6.0e24 
