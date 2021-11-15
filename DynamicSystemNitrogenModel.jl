c87bfaa58bff1eb858efbddee92ad16c0b45f596
andrew schweitzer <andrewschweitzer0@gmail.com>
2021-11-14 19:43:28 -0500

    fixed files in git

--- DynamicSystemNitrogenModel.jl
+++ DynamicSystemNitrogenModel.jl
@@ -1,66 +1,33 @@
 #####--------------------------------------------------------------------------------#####
 #####--------------------------------------------------------------------------------#####
 
+
 using Pkg
 Pkg.add("Classes")
 Pkg.add("Parameters")
 Pkg.add("DataFrames")
 Pkg.add("StaticArrays")
 Pkg.add("CSV")
-Pkg.add("ECharts")
 Pkg.add("Plots")
+Pkg.add("PyPlot")
 
-#                   --------------------------------------------------                   #
 
 using Parameters
 using Classes
 using DataFrames
 using DelimitedFiles
 using CSV
-using ECharts
 using StaticArrays
 using Plots
+using PyPlot
 
-
-#                   --------------------------------------------------                   #
 
 include("src/classes.jl")
 include("src/Processes.jl")
 include("Visualization/Plot.jl")
 
-#####--------------------------------------------------------------------------------#####
-#####--------------------------------------------------------------------------------#####
 
-print("Welcome to the Elemental Cycling Model!",
-      "\nPlease Indicate from the below list what to do next:",
-      "\n1 : Need inputs",
-      "\n2 : Input variables")
 
-option = readline()
-option = parse(Int64,option)
-
-if option == 1
-
-    print("The inputs for the model are:",
-          "TFinal = 0 [total years for model to run]",
-          "t = 0 [starting time]",
-          "CSV = false [true will write data to csv in output folder]")
-
-else
-
-    print("Please indicate the time length for model as a positive integer:")
-    years = readline()
-    years = parse(Int64,years)
-    
-    print("Please choose whether to write to csv [true,false]:")
-    print_csv = readline()
-
-    print("Beginning Model...")
-    RunModel(years)
-    
-    print("Model complete please look at output folder for data and plots.")
-end 
-    
 #####--------------------------------------------------------------------------------#####
 #####--------------------------------------------------------------------------------#####
 
@@ -91,10 +58,10 @@ function initialize()
     Planet.NMass = log10(10^Planet.ocean.NMass + 10^Planet.crust.NMass + 
                          10^Planet.mantle.NMass + 10^Planet.atmosphere.NMass)
 
-    Planet.ocean.Nfraction = 10^(Planet.ocean.NMass - Planet.NMass)
-    Planet.crust.Nfraction = 10^(Planet.crust.NMass - Planet.NMass)
-    Planet.mantle.Nfraction = 10^(Planet.mantle.NMass - Planet.NMass)
-    Planet.atmosphere.Nfraction = 10^(Planet.atmosphere.NMass - Planet.NMass)
+    Planet.ocean.Nfraction = 10^Planet.ocean.NMass / 10^Planet.NMass
+    Planet.crust.Nfraction = 10^Planet.crust.NMass / 10^Planet.NMass
+    Planet.mantle.Nfraction = 10^Planet.mantle.NMass / 10^Planet.NMass
+    Planet.atmosphere.Nfraction = 10^Planet.atmosphere.NMass / 10^Planet.NMass
 
     return Monitor,Planet
 end
@@ -142,8 +109,6 @@ function RunModel(years::Int64)
     t += 1 # initial values stored in timestep 0 so first that t represents
            # completed years
     
-    print("\n t:",t)
-    print("\n years:",years)
 
     
     while t < years
@@ -154,38 +119,46 @@ function RunModel(years::Int64)
         t += 1
         
 
-        TenPercent = convert(Int64,round(years/10))
-        print("\n t:",t)
+        TenPercent = ceil(years/10)
 
 
 
         if t == TenPercent
             #allow used to check model progression
-            print("\n10% complete...\n")
-        elseif t == 2*TenPercent
+            print("\n10% complete...",
+                  "\n",t,"years\n")
+        elseif t == ceil(2*TenPercent)
             #allow used to check model progression
-            print("\n20% complete...\n")
-        elseif t == 3*TenPercent
+            print("\n20% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(3*TenPercent)
             #allow used to check model progression
-            print("\n30% complete...\n")
-        elseif t == 4*TenPercent
+            print("\n30% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(4*TenPercent)
             #allow used to check model progression
-            print("\n40% complete...\n")
-        elseif t == 5*TenPercent
+            print("\n40% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(5*TenPercent)
             #allow used to check model progression
-            print("\n50% complete...\n")
-        elseif t == 6*TenPercent
+            print("\n50% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(6*TenPercent)
             #allow used to check model progression
-            print("\n60% complete...\n")
-        elseif t == 7*TenPercent
+            print("\n60% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(7*TenPercent)
             #allow used to check model progression
-            print("\n70% complete...\n")
-        elseif t == 8*TenPercent
+            print("\n70% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(8*TenPercent)
             #allow used to check model progression
-            print("\n80% complete...\n")
-        elseif t == 9*TenPercent
+            print("\n80% complete...\n",
+            "\n",t,"years\n")
+        elseif t == ceil(9*TenPercent)
             #allow used to check model progression
-            print("\n90% complete...\n")
+            print("\n90% complete...\n",
+            "\n",t,"years\n")
         else
             # Visualize if the model has completed the proper number of runs
             #= if t == TFinal
@@ -196,8 +169,8 @@ function RunModel(years::Int64)
                 return print("Model Complete")
             end =#
             if t == years
-                visualize(years,Monitor)
-                CSV.write("/home/andrew/Desktop/Elemental-Cycling-Biomass/Outputs/Data.csv",Monitor)
+                #visualize(years,Monitor)
+                CSV.write("\n\n/home/andrew/Desktop/Elemental-Cycling-Biomass/Outputs/Data.csv",Monitor)
                 print("\n\nModel Complete\n")
                 print("\nModel data saved in /Outputs/Data.csv\n\n")
                 return Planet,Monitor
@@ -205,5 +178,39 @@ function RunModel(years::Int64)
         end
     end
 end
+
+#####--------------------------------------------------------------------------------#####
+#####--------------------------------------------------------------------------------#####
+
+#   Planet,Monitor = RunModel(1000)
+
+#####--------------------------------------------------------------------------------#####
+#####--------------------------------------------------------------------------------#####
+
+print("Welcome to the Elemental Cycling Model!",
+      "\nPlease Indicate from the below list what to do next:",
+      "\n1 : Need inputs",
+      "\n2 : Input variables\n\n")
+
+option = readline()
+option = parse(Int64,option)
+
+if option == 1
+
+    print("The inputs for the model are:",
+          "\nTFinal = 0 [total years for model to run]",
+          "\nInstallPackage = true")
+
+else
+
+    print("\nPlease indicate the time length for model as a positive integer:")
+    years = readline()
+    years = parse(Int64,years)
+
+    print("\nBeginning Model...\n")
+    RunModel(years)
+    
+    print("\n\nModel complete please look at Output folder for data and plots.")
+end 
 
 #                   --------------------------------------------------                   #
--- Visualization/Plot.jl
+++ Visualization/Plot.jl
@@ -1,30 +1,56 @@
-#                   --------------------------------------------------                    #
+#####--------------------------------------------------------------------------------#####
+#####--------------------------------------------------------------------------------#####
+
+
+using Pkg
+Pkg.add("Classes")
+Pkg.add("Parameters")
+Pkg.add("DataFrames")
+Pkg.add("StaticArrays")
+Pkg.add("CSV")
+Pkg.add("Plots")
+Pkg.add("PyPlot")
+
+
+using Parameters
+using Classes
+using DataFrames
+using DelimitedFiles
+using CSV
+using StaticArrays
+using Plots
+using PyPlot
+
+
+#####--------------------------------------------------------------------------------#####
+#####--------------------------------------------------------------------------------#####
 
 function visualize(years,Monitor)
 
-    #PlotSteps = range(1,convert(Int64,years),step = convert(Int64,round(years/10)) )
-
     VisualizeNFractionOverTime(Monitor)
     VisualizeNMassOverTime(Monitor)
-    #VisualizeGlobalNDistribution(PlotSteps,Monitor)
-    
 
 end
 
 #                   --------------------------------------------------                    #
 
 function VisualizeNFractionOverTime(Monitor)
-    NitrogenDistributionPlot = bar(Monitor.time, 
-                                   hcat(Monitor.AtmosphereNFraction, 
-                                        Monitor.OceanNFraction, 
-                                        Monitor.CrustNFraction,
-                                        Monitor.MantleNFraction), 
-                                   color = ["grey", "blue", "brown","red"], 
-                                   stack = true)
-    title!(NitrogenDistributionPlot, 
-           text = "Nitrogen Fraction of Global Nitrogen mass by Resevoir Time Series")
-    filename = "/home/andrew/Desktop/Elemental-Cycling-Biomass/Outputs/NitrogenDistributionPlot.png"
-    savefig(filename,NitrogenDistributionPlot)         
+
+    PyPlot.clf()
+    P1 = Plots.plot(Monitor.time,Monitor.CrustNFraction,title = "Crust",legend = false)
+    gcf()
+    P2 = Plots.plot(Monitor.time,Monitor.OceanNFraction,title = "Ocean",legend = false)
+    gcf()
+    P3 = Plots.plot(Monitor.time,Monitor.AtmosphereNFraction,title = "Atmosphere",legend = false)
+    gcf()
+    P4 = Plots.plot(Monitor.time,Monitor.MantleNFraction,title = "Mantle",legend = false)
+    gcf()
+    PyPlot.clf()
+    subplot = Plots.plot(P1,P2,P3,P4,
+                layout = 4)
+
+
+    savefig("/home/andrew/Desktop/Elemental-Cycling-Biomass/Outputs/NitrogenFractionOverTime.png",subplot)          
 
 end
 
@@ -35,43 +61,23 @@ function VisualizeNMassOverTime(Monitor)
     # Looks at the mass of Nitrogen in each resevoir and plots it along the time step.
     # This is the Nitrogen Mass by Reservoir Time Series
 
-    NitogenMassPlot = line(Monitor[time], hcat(Monitor.AtmosphereNMass, 
-                                                Monitor.OceanNMass, 
-                                                Monitor.CrustNMass,
-                                                Monitor.MantleNMass) )
+    PyPlot.clf()
+    P1 = plot(Monitor.time,Monitor.CrustNMass,title = "Crust",legend = false)
+    gcf()
+    P2 = plot(Monitor.time,Monitor.OceanNMass,title = "Ocean",legend = false)
+    gcf()
+    P3 = plot(Monitor.time,Monitor.AtmosphereNMass,title = "Atmosphere",legend = false)
+    gcf()
+    P4 = plot(Monitor.time,Monitor.MantleNMass,title = "Mantle",legend = false)
+    gcf()
+    PyPlot.clf()
+    subplot = plot(P1,P2,P3,P4,
+                layout = 4)
 
-    title!(NitogenMassPlot, text = "Nitrogen Mass by Resevoir Time Series")
-    filename = "/Outputs/NitogenMassPlot.png"
-    savefig(NitogenMassPlot,filename)
+
+    png("/home/andrew/Desktop/Elemental-Cycling-Biomass/Outputs/NitrogenMassOverTime.png",subplot)          
     
 end
 
 #                   --------------------------------------------------                    #
 
-function VisualizeGlobalNDistribution(PlotSteps,Monitor)
-
-    # pie plot to show distribution at each 10% time step.
-    # useful to view as a series to see larger time shifts 
-    # and its influence on Nitrogen distribution
-
-    for step in PlotSteps
-        filename = "/Outputs/GlobalNitrogenDistribution" + string(step) + ".png"
-
-        x = ["Atmosphere","Ocean","Crust","Mantle"]
-
-        y = [Monitor[AtmosphereNFraction][step],
-             Monitor[OceanNFraction][step],
-             Monitor[CrustNFraction][step],
-             Monitor[MantleNFraction][step]]
-        
-        pie_chart = pie(x,y)
-        title!(pie_chart, 
-               text = "Global Nitrogen Distribution by Resevoir",
-               subtext = "Year: " + str(step))
-        savefig(pie_chart,filename)
-
-    end
-
-end
-
-#                   --------------------------------------------------                    #
